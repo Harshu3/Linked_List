c09665e353a2b67da397af9222400681ceedd8f4
Harshita <chandaliyaharshita222@gmail.com>
2023-05-08 22:40:09 +0530

    Create a simple Linkedlist

---
+++ Linked_List/Custom_LinkedList.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Linked_List
+{
+    public class Custom_LinkedList
+    {
+        public Node head;
+
+        public void AddLast(int data)
+        {
+            Node newNode = new Node(data);
+            if (head == null)
+            {
+                head = newNode;
+                Console.WriteLine("{0} node is added into linkedlist", newNode.data);
+            }
+            else
+            {
+                Node temp = GetLastNode();
+                temp.next = newNode;
+                Console.WriteLine("{0} node is added into linkedlist", newNode.data);
+            }
+        }
+
+        public void Display()
+        {
+            if (head == null)
+            {
+                Console.WriteLine("Linkedlist is empty");
+            }
+            else
+            {
+                Node temp = head;
+                Console.WriteLine("\nNodes are:");
+                while (temp != null)
+                {
+                    Console.Write(temp.data + " ");
+                    temp = temp.next;
+                }
+            }
+        }
+        private Node GetLastNode()
+        {
+            Node temp = head;
+            while (temp.next != null)
+            {
+                temp = temp.next;
+            }
+            return temp;
+        }
+    }
+}
--- Linked_List/Program.cs
+++ Linked_List/Program.cs
@@ -7,6 +7,11 @@ namespace Linked_List
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Data Structure program!");
+            Custom_LinkedList custom_LinkedList = new Custom_LinkedList();
+            custom_LinkedList.AddLast(56);
+            custom_LinkedList.AddLast(30);
+            custom_LinkedList.AddLast(70);
+            custom_LinkedList.Display();
         }
     }
 }
