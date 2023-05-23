b4fdaa90b2cf270af2e881a97fb7b699cd291d7c
Harshita <chandaliyaharshita222@gmail.com>
2023-05-23 18:10:02 +0530

    Removing first node from linked list

--- Linked_List/Program.cs
+++ Linked_List/Program.cs
@@ -7,13 +7,13 @@ namespace Linked_List
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Data Structure program!");
-            Console.WriteLine("Appending LinkedList!");
+            Console.WriteLine("Remove first node from LinkedList!");
             Custom_LinkedList custom_LinkedList = new Custom_LinkedList();
             custom_LinkedList.AddLast(56);
+            custom_LinkedList.AddLast(30);
             custom_LinkedList.AddLast(70);
             custom_LinkedList.Display();
-            custom_LinkedList.Insert(2, 30);
-            Console.WriteLine("After 30 node is added into linked list");
+            custom_LinkedList.RemoveFirst();
             custom_LinkedList.Display();
         }
     }
